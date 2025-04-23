ae6adbc4bd2968dd49dcfeaeb4ca918d5d06049f
SrGordoH <hector.gordo.hernando@gmail.com>
2025-04-23 15:29:00 +0200

    Cambio en el nombre de la ventana

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ int main(int argc,char* argv[])
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(800,600);
-	glutCreateWindow("MiJuego");
+	glutCreateWindow("Enroque Digital");
 
 	//habilitar luces y definir perspectiva
 	glEnable(GL_LIGHT0);
