77ba06ab08c4d65d2f8e72e0b6e1a81a0c831fa4
Jaimeeeeeeee04 <jaime.sanchez.polo@alumnos.upm.es>
2025-05-03 19:55:59 +0200

    Movimientos posibles dama

--- src/Dama.h
+++ src/Dama.h
@@ -1,9 +1,17 @@
 #pragma once
 #include "Pieza.h"
+#include "Tablero_logica.h"
+#include <vector>
+using namespace std;
+class Tablero_logica;
+
 
 class Dama : public Pieza {
 public:
     Dama(const Tablero& t, bool color)
         : Pieza(tipo_t::DAMA, t, color, 10) {}//Constructor con herencia de Pieza para un tablero y color constante
+
+    vector <Posicion> movimientosValidos(Tablero_logica& tab) const;
+
 };
 
