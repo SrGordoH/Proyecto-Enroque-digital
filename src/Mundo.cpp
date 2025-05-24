0f28c77741369496cb216362e2c85d6c10693e48
Jaimeeeeeeee04 <jaime.sanchez.polo@alumnos.upm.es>
2025-05-24 19:05:26 +0200

    Retoques en mundo::clicPos para que las piezas se muevan (de momento no respetan turnos)

--- src/Mundo.cpp
+++ src/Mundo.cpp
@@ -31,23 +31,27 @@ void Mundo::clicPos(int button, int state, int x, int y) {
 			logica.setearPosicionesIniciales(menus.get_modo());
 		}
 		else if (menus.get_menu() == JUEGO) {
-			casilla_clic = pos_clic.coords_to_casilla();
-			centro_casilla_clic = casilla_clic.centro_en_coords();
+			Pieza* seleccionada = tablero.get_pieza_selec(); // obtenemos la pieza actualmente seleccionada
+			if (seleccionada == nullptr) {
+				casilla_clic = pos_clic.coords_to_casilla();
+				centro_casilla_clic = casilla_clic.centro_en_coords();
 
-			std::cout << "Que es la casilla: (" << casilla_clic.fil << ", " << casilla_clic.col << ")\n";
-			std::cout << "Cuyo centro esta en: (" << centro_casilla_clic.x << ", " << centro_casilla_clic.y << ")\n";
-			
-			pieza_clic = logica.obtenerPieza(casilla_clic);
-			if (pieza_clic != nullptr)
-				pieza_clic->movimientosValidos(logica);
-			tablero.set_pieza_selec(pieza_clic);
+				std::cout << "Que es la casilla: (" << casilla_clic.fil << ", " << casilla_clic.col << ")\n";
+				std::cout << "Cuyo centro esta en: (" << centro_casilla_clic.x << ", " << centro_casilla_clic.y << ")\n";
+
+				pieza_clic = logica.obtenerPieza(casilla_clic);
+				if (pieza_clic != nullptr) {
+					pieza_clic->movimientosValidos(logica);
+				}
+				tablero.set_pieza_selec(pieza_clic); //actualizamos pieza seleccionada
+			}
+			else {
+				casilla_clic = pos_clic.coords_to_casilla();
+				bool ok = logica.moverPieza(seleccionada, casilla_clic); // se intenta mover usando la logica ya implementada
+				tablero.set_pieza_selec(nullptr); // limpiamos la seleccion tras el intento
+			}
 
 		}
-		
-		
-
-		
-
 	}
 }
 
