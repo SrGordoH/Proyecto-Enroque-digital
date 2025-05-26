3a72ea033849f84bf08e87546df74d646410d576
Jaimeeeeeeee04 <jaime.sanchez.polo@alumnos.upm.es>
2025-05-26 14:50:18 +0200

    Mejor toma de decisiones IA

--- src/IA.cpp
+++ src/IA.cpp
@@ -1,6 +1,8 @@
 #include "IA.h"
 #include <cmath>
 #include <iostream>
+#include <vector>
+#include "Tablero_logica.h"
 
 int IA::valorPieza(Pieza::tipo_t tipo) const {
     if (tipo == Pieza::tipo_t::PEON) return 1;
@@ -48,25 +50,26 @@ int IA::evaluarMovimiento(Pieza* pieza, Posicion destino, bool color) const {
     return puntaje;
 }
 
-void IA::elegirMejorMovimiento(bool color, Pieza*& mejorPieza, Posicion& mejorDestino) {
-    int mejorPuntaje = -10000;
-    mejorPieza = nullptr;
-    mejorDestino = { -1, -1 };
+void IA::elegirMejorMovimientoFacil(bool color, Pieza*& mejorPieza, Posicion& mejorDestino) {
+    int mejorPuntaje = -1000000;
+    Pieza* mejorP = nullptr;
+    Posicion mejorD{ -1, -1 };
 
-    for (Pieza* pieza : logica->getPiezas()) {
-        if (!pieza || pieza->getColor() != color || !pieza->getPos().esValida()) continue;
-
-        std::vector<Posicion> movimientos = pieza->movimientosValidos(*logica);
-
-        for (const Posicion& destino : movimientos) {
-            int puntaje = evaluarMovimiento(pieza, destino, color);
-            if (puntaje > mejorPuntaje) {
-                mejorPuntaje = puntaje;
-                mejorPieza = pieza;
-                mejorDestino = destino;
+    for (Pieza* p : logica->getPiezas()) {
+        if (!p || p->getColor() != iaColor) continue;   //Evitamos piezas rivales piezas o puntero sin pieza
+        auto movs = p->movimientosValidos(*logica);
+        for (auto& d : movs) {                          //recorremos movimientos 
+            if (!logica->moverPieza(p, d)) continue;    //Evitamos si el puntero al tablero logico no esta correctamente y movemos 
+            int val = AnalisisBasico();                    
+            logica->deshacerUltimoMovimiento();         //Deshacemos tras evaluar    
+            if (val > mejorPuntaje) {
+                mejorPuntaje = val;
+                mejorP = p;
+                mejorD = d;
             }
         }
     }
+    if (mejorP) logica->moverPieza(mejorP, mejorD);    //Devolvemos lo mejor
 }
 
 
@@ -83,3 +86,34 @@ bool IA::hayCapturasDisponibles(bool color) const {
     return false;
 }
 
+int IA::AnalisisBasico() const {
+    int score = 0;
+    for (Pieza* p : logica->getPiezas()) {
+        int v = valorPieza(p->getTipo());
+        // sumamos valores piezas 
+        if (p->getColor() == iaColor) score += v;
+        else score -= v;   //asignamos puntuacion
+    }
+    for (Pieza* p : logica->getPiezas()) {
+        if (!p || p->getColor() != iaColor) continue;               // solo enemigas
+        auto posEnemiga = p->getPos();
+        for (Pieza* mia : logica->getPiezas()) {
+            if (!mia || mia->getColor() != iaColor) continue;      // solo propias
+            auto posMia = mia->getPos();
+            int df = abs(posMia.fil - posEnemiga.fil);
+            int dc = abs(posMia.col - posEnemiga.col);
+            float dist = df + dc;                                 // Distancia en 2 rectas
+
+            int valorPresa = valorPieza(p->getTipo());
+            score += (valorPresa * 2) / (1 + dist);               // Formula para aumentar valor segun te acercas a piezas valiosas  
+        }
+    }
+
+    return score;
+}
+
+int IA::AnalisisProfundo(bool turno, int profundidad) {
+   
+        //2iteraciones de analisis basico
+    
+}
--- src/IA.h
+++ src/IA.h
@@ -7,13 +7,18 @@
 class IA {
 private:
     Tablero_logica* logica;
-
+    bool iaColor = 0;
     int valorPieza(Pieza::tipo_t tipo) const;
     float distancia(Posicion a, Posicion b) const;
     int evaluarMovimiento(Pieza* pieza, Posicion destino, bool color) const;
     bool esMovimientoDeCaptura(Pieza* pieza, Posicion destino) const;
+    int AnalisisBasico() const;
+    int AnalisisProfundo(bool turno, int profundidad);
+
 
 public:
-    void elegirMejorMovimiento(bool color, Pieza*& mejorPieza, Posicion& mejorDestino);
+    void elegirMejorMovimientoFacil(bool color, Pieza*& mejorPieza, Posicion& mejorDestino);
+    void elegirMejorMovimientoDificil(bool color, Pieza*& mejorPieza, Posicion& mejorDestino);
+
     bool hayCapturasDisponibles(bool color) const;
 };
